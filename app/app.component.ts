c7b125d870476d1bc2c91b2e082f93ce7e73983b
Christian Blab <chris.blab@gmail.com>
2016-05-27 09:43:53 +0200

    prepare heroes rendering

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,21 +8,15 @@ export class Hero {
 @Component({
     selector: 'my-app',
     template:`
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+        <li *ngFor="let hero of heroes">
+    </ul>
     `
 })
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    heroes = HEROES;
 }
 
 var HEROES: Hero[] = [
