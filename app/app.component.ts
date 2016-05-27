96dd303231e0d79799d9e71c0a3057dcdde991b2
Christian Blab <chris.blab@gmail.com>
2016-05-27 08:39:31 +0200

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,35 @@
+import {Component} from 'angular2/core';
+
+interface Hero {
+    id: number;
+    name: string;
+}
+
+@Component({
+    selector: 'my-app',
+    template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+    </div>
+    `
+})
+
+export class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+
+
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license
+ */
---
+++ app/main.js
@@ -0,0 +1,21 @@
+System.register(['angular2/platform/browser', './app.component'], function(exports_1) {
+    var browser_1, app_component_1;
+    return {
+        setters:[
+            function (browser_1_1) {
+                browser_1 = browser_1_1;
+            },
+            function (app_component_1_1) {
+                app_component_1 = app_component_1_1;
+            }],
+        execute: function() {
+            browser_1.bootstrap(app_component_1.AppComponent);
+        }
+    }
+});
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license
+ */ 
+//# sourceMappingURL=main.js.map
